0672da3986dcc2362d3d5279974c19833dedba05
mgai <ming.gai@gmail.com>
2023-09-17 22:19:29 +0800

    Exercise 2.1 done.

---
+++ Exercises/ex2_1.py
@@ -0,0 +1,82 @@
+import os
+import tracemalloc
+
+DATA_FILE = os.path.sep.join(["..", "Data", "ctabus.csv"])
+
+
+# A tuple
+# row_tup = (route, date, daytype, rides)
+def read_into_tuple(route, date, daytype, rides):
+    return (route, date, daytype, rides)
+
+
+# A dictionary
+# row_dict = {"route": route, "date": date, "daytype": daytype, "rides": rides}
+def read_into_dict(route, date, daytype, rides):
+    return {"route": route, "date": date, "daytype": daytype, "rides": rides}
+
+
+# A class
+class RowClass:
+    def __init__(self, route, date, daytype, rides) -> None:
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+
+def read_into_class(route, date, daytype, rides):
+    return RowClass(route, date, daytype, rides)
+
+
+# A named tuple
+from collections import namedtuple
+
+RowNamedTuple = namedtuple("Row", ["route", "date", "daytype", "rides"])
+
+# A named tuple with Typing
+import typing
+
+
+class RowClassTyping(typing.NamedTuple):
+    route: str
+    date: str
+    daytype: str
+    rides: int
+
+
+def read_into_named_tuple(route, date, daytype, rides):
+    return RowNamedTuple(route=route, date=date, daytype=daytype, rides=rides)
+
+
+# A class with slots
+class RowClassSlots:
+    __slots__ = ["route", "date", "daytype", "rides"]
+
+    def __init__(self, route, date, daytype, rides) -> None:
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+
+def read_into_class_with_slots(route, date, daytype, rides):
+    return RowClassSlots(route, date, daytype, rides)
+
+
+def read_file(file, method):
+    with open(DATA_FILE, "r") as f:
+        next(f)
+        tracemalloc.start()
+        my_array = []
+        for line in f.readlines():
+            row = line.strip().split(",")
+            my_array.append(method(row[0], row[1], row[2], int(row[3])))
+        current, peak = tracemalloc.get_traced_memory()
+        print(
+            f"{len(my_array)} records read. Current memory: {current:.2f}, Peak: {peak:.2f}."
+        )
+
+
+# Per test, the memory footprint is as below for peak
+# Class > Named Tuple > Tuple > Class with Slots
